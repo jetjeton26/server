ae74704255327e8659c6fbb5cefc4efc328aa28c
Maxence Lange <maxence@artificial-owl.com>
2025-07-16 15:14:18 -0100

    feat(lexicon): marking unstable as @deprecated
    Signed-off-by: Maxence Lange <maxence@artificial-owl.com>

---
+++ lib/unstable/Config/Lexicon/ConfigLexiconStrictness.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * SPDX-FileCopyrightText: 2024 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+
+namespace NCU\Config\Lexicon;
+
+/**
+ * Strictness regarding using not-listed config keys
+ *
+ * - **ConfigLexiconStrictness::IGNORE** - fully ignore
+ * - **ConfigLexiconStrictness::NOTICE** - ignore and report
+ * - **ConfigLexiconStrictness::WARNING** - silently block (returns $default) and report
+ * - **ConfigLexiconStrictness::EXCEPTION** - block (throws exception) and report
+ *
+ * @experimental 31.0.0
+ * @deprecated
+ */
+enum ConfigLexiconStrictness {
+	/**
+	 * @experimental 31.0.0
+	 * @deprecated
+	 */
+	case IGNORE; // fully ignore
+	/**
+	 * @experimental 31.0.0
+	 * @deprecated
+	 */
+	case NOTICE; // ignore and report
+	/**
+	 * @experimental 31.0.0
+	 * @deprecated
+	 */
+	case WARNING; // silently block (returns $default) and report
+	/**
+	 * @experimental 31.0.0
+	 * @deprecated
+	 */
+	case EXCEPTION; // block (throws exception) and report
+}
